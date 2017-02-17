57f64e248f828b2589aca7e002a379ca3dbda982
Doyen Brown <doyen.brown@gmail.com>
2017-02-17 12:10:38 -0500

    add update

--- js/chutes-n-ladders.js
+++ js/chutes-n-ladders.js
@@ -2,11 +2,11 @@ console.log ("please work");
 
 function ChutesnLadders(){
   this.board = [
-    [0, 4, null, null, null],
+    [0, null, 4, null, null],
     [null, null, 4, null, null],
     [null, null, null, 3, null],
     [null, null, null, null, null],
-    [null, 3, null, null, 1],
+    [null, 3,    null, null, 1],
   ];
 
   this.player1="1";
@@ -26,14 +26,11 @@ ChutesnLadders.prototype.movespot = function(randomN) {
       var newRow;
       var newCol;
 
-      if (cell === 1) {
+      if (cell === 1 && stop === 0) {
+        stop = 1;
         console.log("current coords are " + rowIndex + " " + colIndex);
         if (rowIndex === 4 || rowIndex === 2 || rowIndex === 0) {
-          console.log(board[rowIndex][colIndex + randomN]);
-          if (board[rowIndex][colIndex + randomN] === 0){
-            alert("You won!");
-            return;
-          }
+
           if (colIndex - randomN < 0) {
             if (rowIndex===0){
               alert("Nah buddy, try again!");
@@ -41,33 +38,52 @@ ChutesnLadders.prototype.movespot = function(randomN) {
             }
             if (colIndex === 0) {
 
-              console.log("triggered");
               board[rowIndex][colIndex] = null;
               console.log([rowIndex + 1] + " " + [colIndex]);
               isThereALadder = newGame._ladder(board[rowIndex - 1][(randomN - 1)]);
+              isThereAChute = newGame._chute(board[rowIndex - 1][(randomN - 1)]);
               if (isThereALadder === false) {
-                board[rowIndex - 1][(randomN - 1)] = 1;
+                if(isThereAChute===false){
+                  board[rowIndex - 1][(randomN - 1)] = 1;
+                } else {
+                  board[rowIndex][(randomN - 1)] = 1;
+                }
               } else {
                 board[rowIndex - 2][(randomN - 1)] = 1;
               }
-
 
             } else {
               var spot = randomN - (colIndex + 1);
               board[rowIndex][colIndex] = null;
 
               isThereALadder = newGame._ladder(board[(rowIndex - 1)][spot]);
+              isThereAChute = newGame._chute(board[rowIndex - 1][spot]);
               if (isThereALadder === false) {
-                board[(rowIndex - 1)][spot] = 1;
+                  if(isThereAChute===false){
+                    board[(rowIndex - 1)][spot] = 1;
+                  } else {
+                    board[(rowIndex)][spot] = 1;
+                  }
               } else {
                 board[rowIndex - 2][(randomN - 1)] = 1;
               }
             }
           } else {
             isThereALadder = newGame._ladder(board[rowIndex][colIndex - randomN]);
+            isThereAChute = newGame._chute(board[rowIndex][colIndex - randomN]);
+
               if (isThereALadder===false){
-            board[rowIndex][colIndex] = null;
-            board[rowIndex][colIndex - randomN]=1;
+                  if (isThereAChute===false) {
+                    if (randomN <= colIndex && board[rowIndex][colIndex - randomN] === 0){
+                      alert("You won!");
+                    }
+                    board[rowIndex][colIndex] = null;
+                    board[rowIndex][colIndex - randomN]=1;
+                  } else {
+                    board[rowIndex][colIndex] = null;
+                    board[rowIndex+1][colIndex - randomN]=1;
+                  }
+
           } else {
             board[rowIndex][colIndex] = null;
             board[rowIndex-1][colIndex - randomN] = 1;
@@ -77,44 +93,55 @@ ChutesnLadders.prototype.movespot = function(randomN) {
 
         if (rowIndex === 3 || rowIndex === 1) {
           console.log("in odd rows");
-          if (stop === 0) {
             console.log(colIndex + randomN > 4);
             if (colIndex + randomN > 4) {
-              console.log("why");
-              //working on this still
               newRow = (rowIndex - 1);
               newCol = (5 - (randomN - (4 - colIndex)));
+              isThereAChute = newGame._chute(board[newRow][newCol]);
+              if (isThereAChute===false){
+                board[rowIndex][colIndex] = null;
+                board[newRow][newCol] = 1;
+            } else {
               board[rowIndex][colIndex] = null;
-              board[newRow][newCol] = 1;
+              board[newRow + 1][newCol] = 1;
+            }
               //board[rowIndex][colIndex + randomN] = 1;
             } else {
-              console.log("this thing is getting triggered");
-              board[rowIndex][colIndex] = null;
+
               newRow = rowIndex;
               newCol = colIndex + randomN;
-              board[newRow][newCol] = 1;
+              isThereAChute = newGame._chute(board[newRow][newCol]);
+              if (isThereAChute===false){
+                board[rowIndex][colIndex] = null;
+                board[newRow][newCol] = 1;
+            } else {
+              board[rowIndex][colIndex] = null;
+              board[newRow + 1][newCol] = 1;
             }
+              // isThereAChute = newGame._chute(board[newRow][newCol]);
+              //   console.log("chute");
+              // if (isThereAChute===true){
+              //   board[rowIndex][colIndex] = null;
+              //   board[newRow + 1][newCol] = 1;
+              // }
+                }
 
-            isThereAChute = newGame._chute(board[newRow][newCol]);
-
-            stop = 1;
-          }
+            }
         }
-      }
-    });
 
-});
+      });
+
+    });
 };
   //end of move spot
 
   ChutesnLadders.prototype._ladder = function (position) {
      //var board = this.board;
-console.log(position);
-     if (position === null || position===4 || position===0){
-       return false;
-     } else if (position=== 3){
-       alert("Cool, a ladder!")
+     if (position=== 3){
+       alert("Cool, a ladder!");
        return true;
+     } else {
+       return false;
      }
 
 //      this.board.forEach(function (row, rowIndex) {
@@ -139,14 +166,12 @@ console.log(position);
 
  ChutesnLadders.prototype._chute = function (position) {
 
-    if(position ===null){
-      return false;
-    } else if (position ===4){
-      alert("Oh no, a chute!");
-      return true;
-
-    }
-
+   if (position ===4){
+     alert("Oh no, a chute!");
+     return true;
+   } else {
+     return false;
+   }
 
 //    //var board = this.board;
 //    this.board.forEach(function (row, rowIndex) {
